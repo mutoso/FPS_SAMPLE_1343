eb7aed45f839aa7e6652002eac7155418cc00afc
Alastair Feille <me@alastair.se>
2024-04-15 22:55:33 -0500

    Update ammo count on HUD at start

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -22,7 +22,7 @@ public class Gun : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ammo = maxAmmo;
+        AddAmmo(maxAmmo);
     }
 
     // Update is called once per frame
